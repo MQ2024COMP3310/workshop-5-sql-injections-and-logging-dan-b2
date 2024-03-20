218f44da979aa3186f0afa62e83cb7c440bd3032
dan-b2 <daniel.bath@students.mq.edu.au>
2024-03-20 20:18:59 +1100

    lil fix up

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -38,7 +38,7 @@ public class App {
 
     public static boolean isFourLetter(String input) {
 
-        if (input.matches("[a-z]{4,4}") && input.length() == 4) {
+        if (input.matches("[a-z]{4}") && input.length() == 4) {
             return true;
 
         }
@@ -95,28 +95,31 @@ public class App {
             System.out.print("Enter a 4 letter word for a guess or q to quit: ");
             String guess = scanner.nextLine();
 
-            if (isFourLetter(guess) == false) {
-                System.out.println("Sorry, that word is NOT four letters");
-            }
-
-            else {
-
             
 
             while (!guess.equals("q")) {
                 System.out.println("You've guessed '" + guess+"'.");
 
-                if (wordleDatabaseConnection.isValidWord(guess)) { 
-                    System.out.println("Success! It is in the the list.\n");
-                }else{
-                    System.out.println("Sorry. This word is NOT in the the list.\n");
+                if (isFourLetter(guess) == false) {
+                    System.out.println("Sorry, that word is NOT four letters");
                 }
+                else {
+
+                    if (wordleDatabaseConnection.isValidWord(guess)) { 
+                        System.out.println("Success! It is in the the list.\n");
+                    }else{
+                        System.out.println("Sorry. This word is NOT in the the list.\n");
+                    }
+
+                }
+
+                
 
                 System.out.print("Enter a 4 letter word for a guess or q to quit: " );
                 guess = scanner.nextLine();
             }
 
-            }
+            
         } catch (NoSuchElementException | IllegalStateException e) {
             e.printStackTrace();
         }
